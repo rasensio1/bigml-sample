599f23da20fc66a1057df66f1795ebc2ac84d37b
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 13:13:31 -0700

    can hit api

--- restembed/core/models.py
+++ restembed/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class IrisPrediction(models.Model):
+  sepal_length = models.IntegerField(default=0)
+  sepal_width = models.IntegerField(default=0)
+  petal_length = models.IntegerField(default=0)
+  prediction = models.CharField(max_length=50)
+  created_at = models.DateTimeField('date published')
--- restembed/core/views.py
+++ restembed/core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import pdb
+import bigml.api
 
-# Create your views here.
+from .models import IrisPrediction
+from bigml.api import BigML
+
+def index(request):
+  latest_prediction_list = IrisPrediction.objects.order_by('-created_at')[:5]
+  context = {'latest_prediction_list': latest_prediction_list}
+  return render(request, "core/index.html", context)
+
+def new(request):
+  api = BigML(dev_mode=True)
+
+  req = request.POST
+  sepal_l = req['sepal_l']
+  sepal_w = req['sepal_w']
+  petal_l = req['petal_l']
+  model = "model/567620699ed233520200737a"
+
+  pdb.set_trace()
+
+  prediction = api.create_prediction(model, {'sepal length': sepal_l, 'sepal width': sepal_w, 'petal length': pteal_l})
+
+  return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
