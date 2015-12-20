c34dae659a3fe62e91868c7adc7a6a4fb1f46360
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 15:21:05 -0700

    create 200OK test

--- restembed/core/tests.py
+++ restembed/core/tests.py
@@ -1,6 +1,20 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+
+def create_pred(s_l, s_w, p_l, pred, created_at):
+  pred = IrisPrediction(sepal_length=s_l,
+                        sepal_width=s_w,
+                        petal_length=p_l,
+                        prediction=pred,
+                        created_at=timezone.now())
+  return pred
+
 
 class PredictionViewTests(TestCase):
   def test_index_with_manual_prediction(self):
-    self.assertEqual(True, True)
+    response=self.client.get(reverse('core:index'))
+
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "Iris")
 
