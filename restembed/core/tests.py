e04c8eb7b5b81dec7310f4a885bd1b748bf1c1c7
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 14:58:50 -0700

    add check test

--- restembed/core/tests.py
+++ restembed/core/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PredictionViewTests(TestCase):
+  def test_index_with_manual_prediction(self):
+    self.assertEqual(True, True)
+
--- restembed/urls.py
+++ restembed/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^core/', include('restembed.core.urls')),
+    url(r'^', include('restembed.core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
