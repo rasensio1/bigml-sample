c052ca769e453764956c075f6571a901b11b4f40
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 15:24:34 -0700

    add test for previous preds

--- restembed/core/tests.py
+++ restembed/core/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from .models import IrisPrediction
 
-def create_pred(s_l, s_w, p_l, pred, created_at):
-  pred = IrisPrediction(sepal_length=s_l,
+def create_pred(s_l, s_w, p_l, pred):
+  pred = IrisPrediction.objects.create(sepal_length=s_l,
                         sepal_width=s_w,
                         petal_length=p_l,
                         prediction=pred,
@@ -12,9 +13,16 @@ def create_pred(s_l, s_w, p_l, pred, created_at):
 
 
 class PredictionViewTests(TestCase):
-  def test_index_with_manual_prediction(self):
+  def test_index_with_no_prediction(self):
     response=self.client.get(reverse('core:index'))
 
     self.assertEqual(response.status_code, 200)
     self.assertContains(response, "Iris")
+
+  def test_index_shows_past_prediction(self):
+    create_pred(1, 1, 1, 'Iris something')
+
+    response=self.client.get(reverse('core:index'))
+    self.assertContains(response, "something")
+
 
