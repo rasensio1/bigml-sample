48144f460feee7ed67aa11b783d993082daaea66
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 17:23:30 -0700

    add dataset model

--- restembed/core/models.py
+++ restembed/core/models.py
@@ -6,3 +6,11 @@ class IrisPrediction(models.Model):
   petal_length = models.IntegerField(default=0)
   prediction = models.CharField(max_length=50)
   created_at = models.DateTimeField('date published')
+
+class Dataset(models.Model):
+  data_url = models.CharField(max_length=50)
+  source = models.CharField(max_length=50)
+  dataset = models.CharField(max_length=50)
+  model = models.CharField(max_length=50)
+  fields_count = models.IntegerField(default=0)
+
