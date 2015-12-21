6b8eed347055fdfb448326f795c30c70bde40afb
Ryan Asensio <ryan.asensio@gmail.com>
2015-12-20 18:05:23 -0700

    remove dataset model

--- restembed/core/models.py
+++ restembed/core/models.py
@@ -2,15 +2,8 @@ from django.db import models
 
 class IrisPrediction(models.Model):
   sepal_length = models.IntegerField(default=0)
-  sepal_width = models.IntegerField(default=0)
+  sepal_width  = models.IntegerField(default=0)
   petal_length = models.IntegerField(default=0)
-  prediction = models.CharField(max_length=50)
-  created_at = models.DateTimeField('date published')
-
-class Dataset(models.Model):
-  data_url = models.CharField(max_length=50)
-  source = models.CharField(max_length=50)
-  dataset = models.CharField(max_length=50)
-  model = models.CharField(max_length=50)
-  fields_count = models.IntegerField(default=0)
+  prediction   = models.CharField(max_length=50)
+  created_at   = models.DateTimeField('date published')
 
